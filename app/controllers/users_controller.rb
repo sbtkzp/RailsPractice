ddd16813d4fae96757df2af189109b71321593c9
Shibata Kazuki <d0_n0t_0ften_use_this@yahoo.co.jp>
2015-07-10 16:33:48 +0900

    バリデーションエラー出力処理作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       # save a user.
+      flash[:success] = "Welcome to SampleApp!"
+      redirect_to @user
     else
       render 'new'
     end
